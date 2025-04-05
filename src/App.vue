104a8eacce674254a63b511cc98aff0aa862d0f6
My Mai <my.mai@we-are-mea.com>
2025-04-05 15:54:25 +1300

    Setup vuex, vue-router, tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,12 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div class="p-6 bg-white rounded-lg shadow-md">
+      <h1 class="text-2xl font-bold text-blue-600">
+        Vue + TypeScript + Tailwind CSS
+      </h1>
+      <p class="mt-2">
+        If you can see this styled text, everything is working correctly!
+      </p>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
